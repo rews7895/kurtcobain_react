a13e245adb2c75ff6e7ba8283e6dc4b75ee0a0dd
박준석 <yalvz@naver.com>
2020-07-29 01:28:37 +0900

    info.js 수정

--- src/lib/oauth2/info.js
+++ src/lib/oauth2/info.js
@@ -1,17 +1,14 @@
-export const API_BASE_URL = 'https://api.kurtcobain.co.kr';
 // spring local 8080 port
-// export const API_BASE_URL = 'http://localhost:8080';
-// s3
-export const STORAGE_BASE_URL = 'https://kurt-cobain.s3.ap-northeast-2.amazonaws.com';
+export const API_BASE_URL = 'http://localhost:8080';
+// spring local 8080 port
+export const STORAGE_BASE_URL = 'http://localhost:8080';
 // yarn build
 // export const STORAGE_BASE_URL = 'http://localhost:5000';
 export const ACCESS_TOKEN = 'accessToken';
 export const EXPIRES = 'expires'
 export const USER_INFO = 'userInfo';
-
-export const OAUTH2_REDIRECT_URI = 'https://kurtcobain.co.kr/oauth2/redirect'
 // yarn start
-// export const OAUTH2_REDIRECT_URI = 'http://localhost:3000/oauth2/redirect'
+export const OAUTH2_REDIRECT_URI = 'http://localhost:3000/oauth2/redirect'
 // yarn build
 // export const OAUTH2_REDIRECT_URI = 'http://localhost:5000/oauth2/redirect'
 
